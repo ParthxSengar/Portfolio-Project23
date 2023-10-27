10ee59ffdc47d4322702c117688d7dfd471c8259
NarutoxParth <114332684+NarutoxParth@users.noreply.github.com>
2023-10-28 01:42:14 +0530

    first commit

---
+++ Portfolio/src/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <div>
+      <div className="Navigation">
+        
+        Web Developer <b>ek</b>{" "}here.
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
