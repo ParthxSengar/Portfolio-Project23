a12745e954cb2f4228f51a507fa73b7a7276989a
NarutoxParth <114332684+NarutoxParth@users.noreply.github.com>
2023-10-30 00:49:46 +0530

    File Updates

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,12 +1,18 @@
 import Navibar from "./Navbar";
 import "./App.css";
 import Footer from "./Footer";
+import Projects from "./Projects";
+import Certificates from "./Certificates";
+import Introduction from "./Introduction";
 
 function App() {
   return (
     <div>
       <div>
         <Navibar />
+        <Introduction />
+        <Projects />
+        <Certificates />
         <Footer />
       </div>
     </div>
