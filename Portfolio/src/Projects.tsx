1eee16f5cbb5c5c11bdf06f79d5fc52d5786386c
NarutoxParth <114332684+NarutoxParth@users.noreply.github.com>
2023-11-02 01:29:12 +0530

    files updated

--- Portfolio/src/Projects.tsx
+++ Portfolio/src/Projects.tsx
@@ -1,7 +1,31 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="proj-container">
+      <Container>
+        <Row>
+          <Col className="proj1">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Project 1</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed repellendus, mollitia incidunt delectus temporibus rem consequatur hic fugit molestiae amet aspernatur iusto nam.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className="proj2">Project 2</Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
