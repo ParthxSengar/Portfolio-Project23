be05e5d8cb8e9e9022326c92bebe8b02642ef195
KUNAL MATHUR <89037807+Kk015@users.noreply.github.com>
2023-11-02 00:46:40 +0530

    Revert "Revert "1""
    This reverts commit c3a2373c9b64d9209579a019e8cc7ea290d3f06c.

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,12 +1,13 @@
 import Navibar from "./Navbar";
 import "./App.css";
 import Footer from "./Footer";
-
+import Introduction from "./Introduction";
 function App() {
   return (
     <div>
       <div>
         <Navibar />
+        <Introduction />
         <Footer />
       </div>
     </div>
