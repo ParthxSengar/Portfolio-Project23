49a9d6be65329efa141cbbaa725f48ca7f8605d9
NarutoxParth <114332684+NarutoxParth@users.noreply.github.com>
2023-11-08 01:47:01 +0530

    files updated

--- Portfolio/src/Certificates.tsx
+++ Portfolio/src/Certificates.tsx
@@ -1,7 +1,12 @@
 function Certificates() {
   return (
-    <div>Certificates</div>
-  )
+    <div>
+      <div>Certificates</div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dolorem! Dolorem eius, consequatur enim sunt dicta accusantium labore pariatur deserunt nulla error dolore animi nostrum soluta laborum quidem reiciendis architecto deleniti tempore blanditiis. Quaerat tenetur dolorem sapiente similique saepe iste incidunt repudiandae dolorum. Quo iusto nisi alias aperiam ipsam asperiores blanditiis repellat explicabo soluta at unde maiores omnis aliquam harum doloribus cupiditate molestias quas consequatur porro, eaque cum tempore cumque! Quo iste harum officiis id similique, commodi consequuntur at beatae delectus fugiat asperiores repellendus soluta distinctio saepe doloremque earum accusantium, ad cum vitae nemo molestias molestiae dolores. Dolore, cum facilis.
+      </div>
+    </div>
+  );
 }
 
-export default Certificates
+export default Certificates;
--- Portfolio/src/Footer.tsx
+++ Portfolio/src/Footer.tsx
@@ -1,7 +1,10 @@
 function Footer() {
   return (
-    <div>footer</div>
-  )
+    <div>
+      <div>footer</div>
+      <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, vel! Laboriosam nam aperiam alias nisi magni odio maxime animi eius exercitationem laborum, corporis error delectus cum labore? Facere quae officia, consectetur odit ut voluptatem at cumque dolores, ad quasi quod officiis vel pariatur, sequi voluptas. Adipisci enim quis qui. Quibusdam tenetur officia nam blanditiis repudiandae suscipit, magnam beatae, architecto voluptas dolorum corporis quia autem accusamus rem, ratione recusandae reprehenderit. Iure asperiores illo, nulla in explicabo eveniet ad, mollitia natus vel architecto excepturi. Iusto ut placeat excepturi quod tenetur! Deleniti dolores, facere neque possimus et a placeat assumenda ut autem asperiores?</div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
