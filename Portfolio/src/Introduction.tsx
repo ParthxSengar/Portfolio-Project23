c78d3566dca7e8483572c2fc0b55341e3111cd8b
NarutoxParth <114332684+NarutoxParth@users.noreply.github.com>
2023-11-10 01:51:14 +0530

    files updated

--- Portfolio/src/Introduction.tsx
+++ Portfolio/src/Introduction.tsx
@@ -1,7 +1,27 @@
+// import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Introduction() {
   return (
     <div className="intro-main-div">
-      <div className="intro-div"> Web Developer</div>
+      <Container>
+        <Row>
+          <Col>
+            <div className="intro-div">I am a Web Developer</div>
+          </Col>
+          <Col>
+            <div className="intro-img">
+              <img
+                src="https://pnganime.com/web/image-thumbnails/82/121-lg.png"
+                alt=""
+                width="500px"
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
